b769c67532e2f3f45e185a087b2feedc23baa464
Christopher Erin <chris.erin@gmail.com>
2013-04-03 09:46:31 -0500

    uncompress js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,10 +12,6 @@ ShapeDrawer::Application.configure do
   config.serve_static_assets = false
 
   config.eager_load = true
-  # Compress both stylesheets and JavaScripts
-  config.assets.js_compressor  = :uglifier
-  #config.assets.css_compressor = :scss
-  config.assets.compress = false
 
   # Specifies the header that your server uses for sending files
   # (comment out if your front-end server doesn't support this)
