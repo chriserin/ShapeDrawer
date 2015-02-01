2d05844e0926b76709da7f273f721276ff1e532c
Chris Erin <dev+chriserin@hashrocke.com>
2015-02-01 17:33:32 -0600

    Use env for phantom host conf

--- app/models/image_capture.rb
+++ app/models/image_capture.rb
@@ -4,7 +4,7 @@ require 'capybara/poltergeist'
 
 Capybara.javascript_driver = :poltergeist
 Capybara.run_server = false
-Capybara.app_host = 'http://localhost:8080'
+Capybara.app_host = ENV.fetch("PHANTOM_HOST")
 Capybara.current_driver = :poltergeist
 
 class ImageCapture
